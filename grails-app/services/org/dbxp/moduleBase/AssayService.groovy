87a1d51eaed16602ac6b70b8225dafc000f003ee
vrclvrncknm <s.h.sikkema@gmail.com>
2011-07-18 13:02:14 +0000

    Split getStudiesAndAssaysReadableByUser into getAssaysReadableByUser and getAssaysReadableByUserAndGroupedByStudy

--- grails-app/services/org/dbxp/moduleBase/AssayService.groovy
+++ grails-app/services/org/dbxp/moduleBase/AssayService.groovy
@@ -4,23 +4,18 @@ class AssayService {
 
     static transactional = true
 
-    /**
-     * ...
-     * Only studies with at least one readable assay are returned.
-     *
-     * @param user
-     * @return Map with study objects as keys and assay object lists as values
-     *  e.g.: [Study1: [assay1, assay2], Study2: [assay3, assay4]]
-     */
-    List getStudiesAndAssaysReadableByUser(User user) {
+    List getAssaysReadableByUser(User user) {
 
         // TODO: is 'read' flag always enabled when 'write' or 'isOwner' is?
-        def authorizations = Auth.findAllByUserAndCanRead(user, true)
+        def readEnabledAuthorizations = Auth.findAllByUserAndCanRead(user, true)
 
-        def studiesWithAssays =  authorizations*.study.findAll { it.assays }
-
-        studiesWithAssays.collect { study -> [study: study.assays] }
+        readEnabledAuthorizations*.study.assays.flatten()
 
     }
 
+    Map getAssaysReadableByUserAndGroupedByStudy(User user) {
+
+        getAssaysReadableByUser(user).groupBy { Assay assay -> assay.study }
+
+    }
 }
