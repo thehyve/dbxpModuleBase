55523c74c79e42508b1774556741cf306364826b
vrclvrncknm <s.h.sikkema@gmail.com>
2011-07-27 09:13:08 +0000

    some fixes related to resources plugin in dbxpModuleBase: - included datatables javascript - copied images to project so they get included (and the warnings disappear)

--- grails-app/conf/DbxpModuleBasePluginResources.groovy
+++ grails-app/conf/DbxpModuleBasePluginResources.groovy
@@ -1,10 +1,15 @@
 modules = {
 	moduleBase {
-		dependsOn 'jquery, jquery-ui'
+		dependsOn 'jquery, jquery-ui, datatables'
 
-		resource id:'css', url:[plugin: 'dbxpModuleBase', dir:'css', file: 'datatables.css']
-//		resource id:'css', url:[plugin: 'dbxpModuleBase', dir:'css/cupertino', file: 'jquery-ui-1.8.13.custom.css']
-		resource id:'js', url:[plugin: 'dbxpModuleBase', dir:'js', file: 'topnav.js']
-		resource id:'js', url:[plugin: 'dbxpModuleBase', dir:'js', file: 'datatables.js']
+		resource url:[plugin: 'dbxpModuleBase', dir:'js', file: 'topnav.js']
 	}
+
+    datatables {
+        dependsOn 'jquery-ui'
+
+        resource url:[plugin: 'dbxpModuleBase', dir:'css', file: 'datatables.css']
+        resource url:[plugin: 'jquery-datatables', dir:'js', file:'jquery.dataTables.min.js']
+        resource url:[plugin: 'dbxpModuleBase', dir:'js', file: 'datatables.js']
+    }
 }
