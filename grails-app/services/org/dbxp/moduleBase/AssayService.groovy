2b88ac788a248eef7cac65f0f2b1b343df33941d
michaelvanvliet <info@michaelvanvliet.nl>
2011-08-29 08:39:43 +0000

    Added: Retrieve single Assay by AssayId

--- grails-app/services/org/dbxp/moduleBase/AssayService.groovy
+++ grails-app/services/org/dbxp/moduleBase/AssayService.groovy
@@ -3,6 +3,10 @@ package org.dbxp.moduleBase
 class AssayService {
     static transactional = 'mongo'
 
+	Assay getAssayReadableByUserById(User user, AssayId = null) {
+		return (AssayId == null) ?: getAssaysReadableByUser(user).find { it.id as int == AssayId as int }
+	}
+	
     List getAssaysReadableByUser(User user) {
         def readEnabledAuthorizations = Auth.findAllByUserAndCanRead(user, true)
 
