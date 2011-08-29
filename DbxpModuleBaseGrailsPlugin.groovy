070f5157c344ffc6979ab343e1d4c3291ba9fd9b
vrclvrncknm <s.h.sikkema@gmail.com>
2011-08-29 12:25:00 +0000

    datatables header now resizes together with dialog disabled clicking on assay to reveal dialog for associated file
    upgraded moduleBase to 0.4.4.1

--- DbxpModuleBaseGrailsPlugin.groovy
+++ DbxpModuleBaseGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class DbxpModuleBaseGrailsPlugin {
     // the plugin version
-    def version = "0.4.4"
+    def version = "0.4.4.1"
 	
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
