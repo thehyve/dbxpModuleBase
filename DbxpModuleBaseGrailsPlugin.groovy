b02d28511e8c6dca41882fe364fee8fff6c85172
keesvanbochove <kees@thehyve.nl>
2012-06-25 15:39:11 +0200

    removed dependsOn clause entirely to avoid java.lang.ClassCastException: java.util.ArrayList cannot be cast to java.util.Map at _PluginDependencies_groovy$_run_closure5_closure40.doCall(_PluginDependencies_groovy:270)

--- DbxpModuleBaseGrailsPlugin.groovy
+++ DbxpModuleBaseGrailsPlugin.groovy
@@ -4,7 +4,6 @@ class DbxpModuleBaseGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on are declared in BuildConfig.groovy
-	def dependsOn = []
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp",
