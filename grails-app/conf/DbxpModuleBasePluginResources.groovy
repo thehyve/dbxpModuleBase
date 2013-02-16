7014850c5b68775c338ec5fe15b49d703ad00494
keesvanbochove <kees@thehyve.nl>
2013-02-16 19:38:27 +0100

    change jQuery-UI back to cupertino, version 0.6.1.2

--- grails-app/conf/DbxpModuleBasePluginResources.groovy
+++ grails-app/conf/DbxpModuleBasePluginResources.groovy
@@ -13,5 +13,13 @@ modules = {
         resource url:[plugin: 'jquery-datatables', dir:'js', file:'jquery.dataTables.min.js', disposition: 'head']
         resource url:[plugin: 'dbxpModuleBase', dir:'js', file: 'datatables.js', disposition: 'head']
     }
-
+	
+	overrides { 
+		'jquery-theme' {
+			resource id:'theme',
+				url:[ plugin: 'dbxpModuleBase', dir: 'css/cupertino',
+					  file:'jquery-ui-1.8.13.custom.min.css'],
+				attrs:[media:'screen, projection']
+		}
+	}
 }
